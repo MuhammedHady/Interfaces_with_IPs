114a6e171132b92816fe15a0fc5238341977c800
MuhammedHady <48069413+MuhammedHady@users.noreply.github.com>
2021-09-17 18:15:29 +0200

    Add files via upload

---
+++ Interface_Under_Iterates.py
@@ -0,0 +1,25 @@
+import glob, os
+from ciscoconfparse import CiscoConfParse
+
+directory = r"C:\Users\almasry.net\Desktop\Network Path\Network Automation\Python\Libraries\Ciscoconfparse\Go Through the files - Project"
+myfiles = []
+for filename in os.listdir(directory):
+	suffix=".conf"
+	filex = filename.endswith(suffix)
+	if filex == True :
+		myfiles.append(filename)
+
+for filefile in myfiles:
+	parse = CiscoConfParse("%s"%filefile, syntax='ios', factory=True)
+	print('#########################################')
+	print('\n')
+	print(filefile)
+	print('-----------')
+	for intf_obj in parse.find_objects_w_child('^interface', '^\s+ip address'):
+		intf_name = intf_obj.re_match_typed('^interface\s+(\S.+?)$')                         	# getting the interface that has the statement "ip address"
+		intf_ip_addr = intf_obj.re_match_iter_typed(r'ip\saddress\s(\d+\.\d+\.\d+\.\d+)\s', result_type=str, group=1, default='')    		# getting the ip on the interface that has the statement "ip address"
+		print("{0}: {1}".format(intf_name, intf_ip_addr))										# print the whole line detailed
+		print('\n')
+	print('#########################################')
+
+	
